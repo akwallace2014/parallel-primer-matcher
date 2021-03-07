b9085c756a2244f450af265254593fc27a2afda4
Alisa Wallace <awallace@seattleu.edu>
2021-03-06 16:50:11 -0800

    ReplicationProductTest: tested findAllMatches with expected matches, passed

--- draft1/ReplicationProductTest.java
+++ draft1/ReplicationProductTest.java
@@ -7,6 +7,14 @@ public class ReplicationProductTest {
     
     String t1 = "GCTGCTGCTGCTGCTAAAAAAAAAAAAGCTGCTGCTGCTAAAAAAAAAAAAGCTGCTGCTGCTAAAAAAAAAAAAGCTGCTGCTAAAAAAAAAAAAGCTGCT";
 
+    String t1sub1 = "GCTGCTGCTGCTGCTAAAAAAAAAAAAGCTGCTGCTGCTAAAAAAAAAAAAGCTGCTGCTGCTAAAAAAAAAAAAGCTGCTGCTAAAAAAAAAAAA";
+
+    String t1sub2 = "GCTGCTGCTGCTGCTAAAAAAAAAAAAGCTGCTGCTGCTAAAAAAAAAAAAGCTGCTGCTGCTAAAAAAAAAAAA";
+
+    String t1sub3 = "GCTGCTGCTGCTGCTAAAAAAAAAAAAGCTGCTGCTGCTAAAAAAAAAAAA";
+
+    String t1sub4 = "GCTGCTGCTGCTGCTAAAAAAAAAAAA";
+
     String p1Fwd = "AAAAAAAAAAAA";
     String p1Rev = "TTTTTTTTTTTT";
 
@@ -27,9 +35,48 @@ public class ReplicationProductTest {
         assertSame(primer1Rev, rp1.getPrimer());
     }
 
-    public void testFindAllMatches() {
+    @Test 
+    public void testFindAllMatchesMatches() {
 
+        ReplicationProduct rp1 = new ReplicationProduct(template1, primer1Rev);
 
+        Sequence sub1 = new Sequence("t1subproduct1", t1sub1, false);
+        int loc1 = 6;
+        String expected1 = sub1.complement();
+
+        Sequence sub2 = new Sequence("t1subproduct2", t1sub2, false);
+        int loc2 = 27;
+        String expected2 = sub2.complement();
+
+        Sequence sub3 = new Sequence("t1subproduct3", t1sub3, false);
+        int loc3 = 51;
+        String expected3 = sub3.complement();
+
+        Sequence sub4 = new Sequence("t1subproduct4", t1sub4, false);
+        int loc4 = 75;
+        String expected4 = sub4.complement();
+
+        rp1.findAllMatches();
+
+        assertTrue(rp1.isAnalyzed());
+        assertFalse(rp1.isEmpty());
+        assertEquals(4, rp1.numMatches());
+
+        // first product
+        assertEquals(loc1, rp1.getMatchLocation(0));
+        assertEquals(expected1, rp1.getMatchProduct(0));
+
+        // second product
+        assertEquals(loc2, rp1.getMatchLocation(1));
+        assertEquals(expected2, rp1.getMatchProduct(1));
+
+        // third product
+        assertEquals(loc3, rp1.getMatchLocation(2));
+        assertEquals(expected3, rp1.getMatchProduct(2));
+
+        // fourth product
+        assertEquals(loc4, rp1.getMatchLocation(3));
+        assertEquals(expected4, rp1.getMatchProduct(3));
     }
 
 }
