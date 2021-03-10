57744e38c4cc397cbb524192e13e1e08b5fbf52f
Alisa Wallace <awallace@seattleu.edu>
2021-03-10 13:36:32 -0800

    Reconfigured all ReplicationProduct tests, all pass

--- draft2/ReplicationProductTest.java
+++ draft2/ReplicationProductTest.java
@@ -9,14 +9,6 @@ public class ReplicationProductTest {
 
     String t1 = "GCTGCTGCTGCTGCTAAAAAAAAAAAAGCTGCTGCTGCTAAAAAAAAAAAAGCTGCTGCTGCTAAAAAAAAAAAAGCTGCTGCTAAAAAAAAAAAAGCTGCT";
 
-    String t1sub4 = "GCTGCTGCTGCTGCTAAAAAAAAAAAAGCTGCTGCTGCTAAAAAAAAAAAAGCTGCTGCTGCTAAAAAAAAAAAAGCTGCTGCTAAAAAAAAAAAA";
-
-    String t1sub3 = "GCTGCTGCTGCTGCTAAAAAAAAAAAAGCTGCTGCTGCTAAAAAAAAAAAAGCTGCTGCTGCTAAAAAAAAAAAA";
-
-    String t1sub2 = "GCTGCTGCTGCTGCTAAAAAAAAAAAAGCTGCTGCTGCTAAAAAAAAAAAA";
-
-    String t1sub1 = "GCTGCTGCTGCTGCTAAAAAAAAAAAA";
-
     String p1Fwd = "AAAAAAAAAAAA";      // shouldn't match
     String p1Rev = "TTTTTTTTTTTT";      // should match
 
@@ -25,13 +17,13 @@ public class ReplicationProductTest {
 
     Sequence template1 = new Sequence("template1", t1, five);
 
-    // should throw exception
+    // should throw exception with template1
     Sequence primer1Fwd = new Sequence("primer1Fwd", p1Fwd, five);
 
     // should make no products with template1
     Sequence primer1Rev = new Sequence("primer1Fwd", p1Fwd, three);
 
-    // should find matches with template1
+    // should find 4 matches with template1
     Sequence primer1RC = new Sequence("primer1Rev", p1Rev, three);
 
     @Test
@@ -58,62 +50,128 @@ public class ReplicationProductTest {
     }
 
     @Test 
-    public void testFindAllMatchesMatchesExpected() {
+    public void testFindAllMatchesMatchesExpected5PrimeTemplate() {
+        // Test with template = 5', primer = 3'
 
-        System.out.println(Sequence.generateComplement("CAT"));
-    
+        // Expected products (3' to 5')
+        // String t1sub1 = "GCTGCTGCTGCTGCTAAAAAAAAAAAA";
+        String t1sub1 = "CGACGACGACGACGATTTTTTTTTTTT";
+
+        // String t1sub2 = "GCTGCTGCTGCTGCTAAAAAAAAAAAAGCTGCTGCTGCTAAAAAAAAAAAA";
+        String t1sub2 = "CGACGACGACGACGATTTTTTTTTTTTCGACGACGACGATTTTTTTTTTTT";
+
+        // String t1sub3 = "GCTGCTGCTGCTGCTAAAAAAAAAAAAGCTGCTGCTGCTAAAAAAAAAAAAGCTGCTGCTGCTAAAAAAAAAAAA";
+        String t1sub3 = "CGACGACGACGACGATTTTTTTTTTTTCGACGACGACGATTTTTTTTTTTTCGACGACGACGATTTTTTTTTTTT";
+
+        // String t1sub4 = "GCTGCTGCTGCTGCTAAAAAAAAAAAAGCTGCTGCTGCTAAAAAAAAAAAAGCTGCTGCTGCTAAAAAAAAAAAAGCTGCTGCTAAAAAAAAAAAA";
+        String t1sub4 = "CGACGACGACGACGATTTTTTTTTTTTCGACGACGACGATTTTTTTTTTTTCGACGACGACGATTTTTTTTTTTTCGACGACGATTTTTTTTTTTT";
 
         ReplicationProduct rp1 = new ReplicationProduct("Test 1", template1, primer1RC);
 
-        Sequence expected1 = new Sequence("t1subproduct1", Sequence.generateComplement(t1sub1), three);
-        System.out.println("Expected = " + Sequence.generateComplement(t1sub1));
-        int loc1 = 15;
+        Sequence expected1 = new Sequence("t1subproduct1", t1sub1, three);
+        int loc1 = t1sub1.length() - p1Rev.length();    // 15
 
-        // Sequence sub2 = new Sequence("t1subproduct2", t1sub2, false);
-        // int loc2 = 27;
-        // String expected2 = sub2.complement();
+        Sequence expected2 = new Sequence("t1subproduct2", t1sub2, three);
+        int loc2 = t1sub2.length() - p1Rev.length();
 
-        // Sequence sub3 = new Sequence("t1subproduct3", t1sub3, false);
-        // int loc3 = 51;
-        // String expected3 = sub3.complement();
+        Sequence expected3 = new Sequence("t1subproduct3", t1sub3, three);
+        int loc3 = t1sub3.length() - p1Rev.length();
 
-        // Sequence sub4 = new Sequence("t1subproduct4", t1sub4, false);
-        // int loc4 = 75;
-        // String expected4 = sub4.complement();
+        Sequence expected4 = new Sequence("t1subproduct4", t1sub4, three);
+        int loc4 = t1sub4.length() - p1Rev.length();
+
 
         rp1.findAllMatches();
 
         assertTrue(rp1.isAnalyzed());
         assertFalse(rp1.isEmpty());
         assertEquals(4, rp1.numMatches());
-
-        int rpLoc0 = rp1.getMatchLocation(0);
-        System.out.println(rpLoc0);
-
-        Sequence match1 = rp1.getMatchProduct(0);
-        System.out.println("Direction = " + match1.direction().toString() + ", sequence = " + match1.sequence());
-
+    
         // first product
+        Sequence match1 = rp1.getMatchProduct(0);
         assertEquals(loc1, rp1.getMatchLocation(0));
-        assertEquals(expected1.sequence(), rp1.getMatchProduct(0).sequence());
+        assertEquals(expected1, match1);
 
         // second product
-        // assertEquals(loc2, rp1.getMatchLocation(1));
-        // assertEquals(expected2, rp1.getMatchProduct(1));
+        Sequence match2 = rp1.getMatchProduct(1);
+        assertEquals(loc2, rp1.getMatchLocation(1));
+        assertEquals(expected2, match2);
 
-        // // third product
-        // assertEquals(loc3, rp1.getMatchLocation(2));
-        // assertEquals(expected3, rp1.getMatchProduct(2));
+        // third product
+        Sequence match3 = rp1.getMatchProduct(2);
+        assertEquals(loc3, rp1.getMatchLocation(2));
+        assertEquals(expected3, match3);
 
-        // // fourth product
-        // assertEquals(loc4, rp1.getMatchLocation(3));
-        // assertEquals(expected4, rp1.getMatchProduct(3));
+        // fourth product
+        Sequence match4 = rp1.getMatchProduct(3);
+        assertEquals(loc4, rp1.getMatchLocation(3));
+        assertEquals(expected4, match4);
+
+    }
+
+    @Test 
+    public void testFindAllMatchesMatchesExpected3PrimeTemplate() {
+        // Test with template = 3', primer = 5'
+        
+        // Expected subproducts (5' to 3')
+        String sub1 = "TTTTTTTTTTTTCGACGACGACGATTTTTTTTTTTTCGACGACGACGATTTTTTTTTTTTCGACGACGATTTTTTTTTTTTCGACGA";
+
+        String sub2 = "TTTTTTTTTTTTCGACGACGACGATTTTTTTTTTTTCGACGACGATTTTTTTTTTTTCGACGA";
+
+        String sub3 = "TTTTTTTTTTTTCGACGACGATTTTTTTTTTTTCGACGA";
+
+        String sub4 = "TTTTTTTTTTTTCGACGA";
+
+        Sequence template13P = new Sequence("template1", t1, three);
+        Sequence primer1RC5P = new Sequence("primer1Rev", p1Rev, five);
+
+        ReplicationProduct rp1 = new ReplicationProduct("Test 2", template13P, primer1RC5P);
+
+        Sequence expected1 = new Sequence("t1subproduct1", sub1, five);
+        int loc1 = t1.length() - sub1.length();
+
+        Sequence expected2 = new Sequence("t1subproduct2", sub2, five);
+        int loc2 = t1.length() - sub2.length();
+
+        Sequence expected3 = new Sequence("t1subproduct3", sub3, five);
+        int loc3 = t1.length() - sub3.length();
+
+        Sequence expected4 = new Sequence("t1subproduct4", sub4, five);
+        int loc4 = t1.length() - sub4.length();
+
+        rp1.findAllMatches();
+
+        assertTrue(rp1.isAnalyzed());
+        assertFalse(rp1.isEmpty());
+        assertEquals(4, rp1.numMatches());
+    
+        // first product
+        Sequence match1 = rp1.getMatchProduct(0);
+        System.out.println("Calculated = " + loc1 + ", actual = " + rp1.getMatchLocation(0));
+        assertEquals(loc1, rp1.getMatchLocation(0));
+        assertEquals(expected1, match1);
+
+        // second product
+        Sequence match2 = rp1.getMatchProduct(1);
+        assertEquals(loc2, rp1.getMatchLocation(1));
+        assertEquals(expected2, match2);
+
+        // third product
+        Sequence match3 = rp1.getMatchProduct(2);
+        assertEquals(loc3, rp1.getMatchLocation(2));
+        assertEquals(expected3, match3);
+
+        // fourth product
+        Sequence match4 = rp1.getMatchProduct(3);
+        assertEquals(loc4, rp1.getMatchLocation(3));
+        assertEquals(expected4, match4);
+
     }
 
     @Test
-    public void testFindAllMatchesNoMatches() {
+    public void testFindAllMatchesNoMatchesExpected() {
 
-        ReplicationProduct rp2 = new ReplicationProduct(template1, primer1Fwd);
+        ReplicationProduct rp2 = new ReplicationProduct("Test 3", template1, primer1Rev);
 
         rp2.findAllMatches();
 
@@ -123,15 +181,26 @@ public class ReplicationProductTest {
     }
 
     @Test
-    public void testFindAllMatchesPartialMatch() {
-        Sequence p1partial = new Sequence("t1 partial match", p1RevPartialMatch, false);
-        ReplicationProduct rp2 = new ReplicationProduct(template1, p1partial);
+    public void testFindAllMatchesPartialMatchNoMatchesExpected() {
+        Sequence p1partial = new Sequence("t1PartialMatch", p1RevPartialMatch, three);
+        ReplicationProduct rp2 = new ReplicationProduct("Test 4", template1, p1partial);
 
         rp2.findAllMatches();
 
         assertTrue(rp2.isAnalyzed());
         assertTrue(rp2.isEmpty());
         assertEquals(0, rp2.numMatches());
+    }
+
+    @Test
+    public void testFindAllMatchesException() {
+
+        // sequences have the same directionality so product cannot be made
+        try {
+            ReplicationProduct rp = new ReplicationProduct("Test 5", template1, primer1Fwd);
+            fail();
+        }
+        catch (IllegalArgumentException e) {}
     }
 
     @Test
@@ -139,15 +208,13 @@ public class ReplicationProductTest {
 
         // swap template and primer so matching looks for template sequence
         // in primer sequence
-        ReplicationProduct rp3 = new ReplicationProduct(primer1Rev, template1);
+        ReplicationProduct rp3 = new ReplicationProduct("Exception", primer1Rev, template1);
 
         try {
             rp3.findAllMatches();
             fail();
         }
         catch (IllegalArgumentException e) {}
-
-
     }
 
 }
