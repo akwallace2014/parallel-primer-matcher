abe337c89ee2ad4a7452bdbd12161c060f52e26f
Alisa Wallace <awallace@seattleu.edu>
2021-03-10 16:25:54 -0800

    Made ParallelMatcher and ParallelMatcherTest

---
+++ draft3/ParallelMatcher.java
@@ -0,0 +1,145 @@
+/*
+ * Alisa Wallace
+ * CPSC 5600 Parallel Computing 
+ * Seattle University, WQ 2021 with Kevin Lundeen
+ * Final Project
+ * 
+ * This is free and unencumbered software released into the public domain.
+ */
+
+import java.util.*;
+import java.util.concurrent.TimeUnit;
+
+public class ParallelMatcher {
+    
+    private static final int MIN_LENGTH = 1;
+    private int numThreads;
+    private String template;
+    private String pattern;
+    private ArrayList<Integer> matches;
+    private ArrayList<Thread> threads;
+
+
+    public ParallelMatcher(String template, String pattern, int numThreads) {
+        
+        validateInput(template, pattern);
+
+        this.template = template;
+        this.pattern = pattern;
+        this.numThreads = numThreads;
+        matches = new ArrayList<Integer>();
+        threads = new ArrayList<Thread>();
+    }
+
+
+    /**
+     * Naive string-matching algorithm
+     * Looks for all instances of exact matches between template and pattern
+     * @return a list of start indices in template where there is a match or 
+     * null if there are no matches
+     */
+    public ArrayList<Integer> findMatches() {
+    
+
+        // if (pattern.equals(template)) {
+        //     matches.add(0);
+        //     return matches;
+        // }
+
+        int tLength = template.length();
+    
+        int sectionSize = tLength / numThreads;
+
+        for (int i = 0; i < numThreads; i++) {
+            int start = i * sectionSize;
+            int end;
+            if (i == numThreads)
+                end = tLength;
+            else {
+                end = start + sectionSize;
+            }
+            Thread t = new Thread(new ThreadTask(i + 1, start, end));
+            threads.add(t);
+            t.start();
+        }
+
+        try {
+            for (int i = 0; i < numThreads; i++)
+            threads.get(i).join();
+        }
+        catch (InterruptedException e) {
+            System.out.println("Process interrupted, results nullified");
+            return null;
+        }
+
+        return matches;
+
+    }
+
+    /**
+     * For testing purposes only
+     * @return
+     */
+    public ArrayList<Thread> getThreads() {
+        return threads;
+    }
+
+    private void validateInput(String template, String pattern) throws IllegalArgumentException {
+        if (template == null || pattern == null)
+            throw new IllegalArgumentException("Input strings cannot be null.");
+
+        if (template.isBlank() || pattern.isBlank())
+            throw new IllegalArgumentException("Input strings cannot be blank.");
+        
+        if (template.length() < MIN_LENGTH || pattern.length() < MIN_LENGTH)
+            throw new IllegalArgumentException("Input strings must have length >= " + MIN_LENGTH);
+
+        if (template.length() < pattern.length())
+            throw new IllegalArgumentException("Template must have length >= pattern");
+    }
+
+    class ThreadTask implements Runnable {
+
+        int id;     // thread ID
+        int start;  // first index of template to search
+        int end;    // index in template to search up to
+
+        public ThreadTask(int id, int start, int end) {
+            this.id = id;
+            this.start = start;
+            this.end = end;
+        }
+
+        public void run() {
+            
+            // if (id == 0) {
+            //     try {
+            //         TimeUnit.MILLISECONDS.sleep(100);
+            //     }
+            //     catch (InterruptedException e) {}
+            // }
+            System.out.println("Running from thread " + id);
+
+            int pLength = pattern.length();
+
+            for (int t = start; t <= end - pLength; t++) {
+            
+                int p;      // tracks pattern index
+                for (p = 0; p < pLength; p++) {
+                    if (template.charAt(t + p) != pattern.charAt(p)) 
+                        break;
+                }
+    
+                if (p == pLength) {
+                    // we've found the complete pattern
+                    addMatch(t);
+                } 
+            }
+        }
+
+        private synchronized void addMatch(int location) {
+            System.out.println("Thread " + id + " adding match " + location);
+            matches.add(location);
+        }
+    }
+}
---
+++ draft3/ParallelMatcherTest.java
@@ -0,0 +1,94 @@
+/*
+ * Alisa Wallace
+ * CPSC 5600 Parallel Computing 
+ * Seattle University, WQ 2021 with Kevin Lundeen
+ * Final Project
+ * 
+ * This is free and unencumbered software released into the public domain.
+ */
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+import java.util.*;
+
+public class ParallelMatcherTest {
+    
+    String template1 = "-----xxx------";    // match expected at 5
+    String pattern = "xxx";
+
+    // matches expected at 0, 11, 21, 29
+    String template2 = "xxx--------xxx-------xxx-----xxx"; 
+
+    @Test
+    public void testFindMatchesSingleThread() {
+
+        ParallelMatcher pm = new ParallelMatcher(template1, pattern, 1);
+
+        ArrayList<Integer> matches = pm.findMatches();
+
+        assertEquals(matches.size(), 1);
+
+        int matchLoc = matches.get(0);
+        assertEquals(matchLoc, 5);
+
+        ParallelMatcher pm2 = new ParallelMatcher(template2, pattern, 1);
+        
+        ArrayList<Integer> matches2 = pm2.findMatches();
+        int numMatches = matches2.size();
+
+        assertEquals(numMatches, 4);
+
+        int matchLoc1 = matches2.get(0);
+        int matchLoc2 = matches2.get(1);
+        int matchLoc3 = matches2.get(2);
+        int matchLoc4 = matches2.get(3);
+
+        assertEquals(matchLoc1, 0);
+        assertEquals(matchLoc2, 11);
+        assertEquals(matchLoc3, 21);
+        assertEquals(matchLoc4, 29);
+
+    }
+
+    @Test
+    public void testFindMatchesMultipleThreads() {
+
+        ParallelMatcher pm2 = new ParallelMatcher(template2, pattern, 4);
+        
+        ArrayList<Integer> matches2 = pm2.findMatches();
+        int numMatches = matches2.size();
+
+        assertEquals(numMatches, 4);
+
+        Collections.sort(matches2);
+        int matchLoc1 = matches2.get(0);
+        int matchLoc2 = matches2.get(1);
+        int matchLoc3 = matches2.get(2);
+        int matchLoc4 = matches2.get(3);
+
+        assertTrue(matchLoc1 == 0);
+        assertTrue(matchLoc2 == 11);
+        assertTrue(matchLoc3 == 21);
+        assertTrue(matchLoc4 == 29);
+    }
+
+    @Test
+    public void testFindMatchesMultipleThreadsLargeInput() {
+
+        int patternSize = 10;
+        String pattern = "x".repeat(patternSize);
+        String template = makeTemplate(pattern, 100, 3);
+    }
+
+    private String makeTemplate(String pattern, int sectionLength, int repeats) {
+        String str = "-".repeat(sectionLength - pattern.length());
+        str += pattern;
+        String result = str;
+
+        for (int i = 1; i < repeats; i++) {
+            result += str;
+        }
+        System.out.println(result);
+        return result;
+    }
+}
